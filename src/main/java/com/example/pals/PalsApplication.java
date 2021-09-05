4e97600383818602d6f4c059263000dcf1cfdfd1
huyen15012000 <55137935+huyen15012000@users.noreply.github.com>
2021-09-06 04:46:56 +0700

    Initial commit

---
+++ src/main/java/com/example/pals/PalsApplication.java
@@ -0,0 +1,13 @@
+package com.example.pals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalsApplication.class, args);
+	}
+
+}
